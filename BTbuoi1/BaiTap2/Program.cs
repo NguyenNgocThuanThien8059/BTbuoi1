6d7494d782a81a80c26bed05d46bb1ef2b0fa39a
NguyenNgocThuanThien8059 <143669066+NguyenNgocThuanThien8059@users.noreply.github.com>
2023-09-08 11:11:35 +0700

    Add files via upload

---
+++ BTbuoi1/BaiTap2/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTap2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> studentList = new List<Student>();
+            bool exit = false;
+            while (!exit)
+            {
+                Console.WriteLine(" === MENU === ");
+                Console.WriteLine(" 1.Them sinh vien ");
+                Console.WriteLine(" 2.Hien thi danh sach sinh vien ");
+                Console.WriteLine(" 3.Xuat SV theo khoa CNTT ");
+                Console.WriteLine(" 4.Xuat SV co DTB lon hon 5 ");
+                Console.WriteLine(" 5.Xuat danh sach SV tang dan theo DTB ");
+                Console.WriteLine(" 6.Xuat danh sach SV co DTB lon hon 5 va thuoc khoa CNTT ");
+                Console.WriteLine(" 0. Thoat ");
+                Console.Write(" Chon chuc nang (0 - 6): ");
+                string choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "1":
+                        AddStudent(studentList);
+                        break;
+                    case "2":
+                        DisplayStudentList(studentList);
+                        break;
+                    case "3":
+                        DisplayStudentByFaculty(studentList, "CNTT");
+                        break;
+                    case "4":
+                        DisplayStudentWithHighAverageScore(studentList, 5);
+                        break;
+                    case "5":
+                        SortStudentByAverageScore(studentList);
+                        break;
+                    case "6":
+                        DisplayStudentByFacultyAndScore(studentList, "CNTT", 5);
+                        break;
+                    case "8":
+                        DisplayStudentListWithLoai(studentList);
+                        break;
+                    case "0":
+                        exit = true;
+                        Console.WriteLine(" Ket thuc chuong trinh. ");
+                        break;
+                    default:
+                        Console.WriteLine(" Tuy chon khong hop le. Vui long chon lai. ");
+                        break;
+                }
+                Console.WriteLine();
+            }
+        }
+        static void AddStudent(List<Student> studentList)
+        {
+            Console.WriteLine(" === Nhap thong tin SV: === ");
+            Student student = new Student();
+            student.Input();
+            studentList.Add(student);
+            Console.WriteLine(" Them sinh vien thanh cong! ");
+        }
+        static void DisplayStudentList(List<Student> studentList)
+        {
+            Console.WriteLine(" === Danh sach chi tiet thong tin SV === ");
+            foreach (Student student in studentList)
+            {
+                student.Show();
+            }
+        }
+        static void DisplayStudentByFaculty(List<Student> studentList, string faculty)
+        {
+            Console.WriteLine(" === Danh sach SV thuoc khoa {0} === ", faculty);
+            var students = studentList.Where(s => s.Faculty.Equals(faculty, StringComparison.OrdinalIgnoreCase)).ToList();
+            DisplayStudentList(students);
+        }
+        static void DisplayStudentWithHighAverageScore(List<Student> studentList, float minDTB)
+        {
+            Console.WriteLine(" === Danh sach SV co diem TB >= {0} === ", minDTB);
+            var students = studentList.Where(s => s.AverageScore >= minDTB).ToList();
+            DisplayStudentList(students);
+        }
+        static void SortStudentByAverageScore(List<Student> studentList)
+        {
+            Console.WriteLine(" === Danh sach SV duoc sap xep theo DTB tang dan === ");
+            var sortedStudents = studentList.OrderBy(s => s.AverageScore).ToList();
+            DisplayStudentList(sortedStudents);
+        }
+        static void DisplayStudentByFacultyAndScore(List<Student> studentList, string faculty, float minDTB)
+        {
+            Console.WriteLine(" === Danh sach SV co DTB >= {0} va thuoc khoa {1} === ", minDTB, faculty);
+            var students = studentList.Where(s => s.AverageScore >= minDTB && s.Faculty.Equals(faculty, StringComparison.OrdinalIgnoreCase)).ToList();
+            DisplayStudentList(students);
+        }
+        static void DisplayStudentListWithLoai(List<Student> studentList)
+        {
+            Console.WriteLine(" === Danh sach chi tiet thong tin SV === ");
+            foreach (Student student in studentList)
+            {
+                student.ShowLoai();
+            }
+        }
+    }
+}
---
+++ BTbuoi1/BaiTap2/Student.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTap2
+{
+    class Student
+    {
+        private string studentID;
+        private string fullName;
+        private float averageScore;
+        private string faculty;
+        private string loai;
+        public String StudentID
+        {
+            get => studentID;
+            set => studentID = value;
+        }
+        public String FullName
+        {
+            get => fullName;
+            set => fullName = value;
+        }
+        public float AverageScore
+        {
+            get => averageScore;
+            set => averageScore = value;
+        }
+        public String Faculty
+        {
+            get => faculty;
+            set => faculty = value;
+        }
+        public String Loai
+        {
+            get => loai;
+            set => loai = value;
+        }
+        public Student()
+        {
+        }
+        public Student(string studentID, string fullName, float averageScore, string faculty,string loai)
+        {
+            this.studentID = studentID;
+            this.fullName = fullName;
+            this.averageScore = averageScore;
+            this.faculty = faculty;
+            this.loai = loai;
+        }
+        public void Input()
+        {
+            Console.WriteLine(" Nhap MSSV: ");
+            studentID = Console.ReadLine();
+            Console.WriteLine(" Nhap Ho Ten SV: ");
+            fullName = Console.ReadLine();
+            Console.WriteLine(" Nhap DiemTB: ");
+            averageScore = float.Parse(Console.ReadLine());
+            Console.WriteLine(" Nhap Khoa: ");
+            faculty = Console.ReadLine();
+        }
+        public void Show()
+        {
+            Console.WriteLine(" MSSV: {0} | HoTen: {1} | Khoa: {2} | DiemTB: {3}", studentID, fullName, faculty, averageScore);
+        }
+        public void ShowLoai()
+        {
+            if(averageScore >= 9 && averageScore <= 10)
+            {
+                Loai = "Xuat Sac";
+            }
+            if (averageScore >= 8 && averageScore < 9)
+            {
+                Loai = " Gioi";
+            }
+            if (averageScore >= 7 && averageScore < 8)
+            {
+                Loai = "Kha";
+            }
+            if (averageScore >= 5 && averageScore < 7)
+            {
+                Loai = "Trung Binh";
+            }
+            if (averageScore >= 4 && averageScore < 5)
+            {
+                Loai = "Yeu";
+            }
+            if (averageScore < 4)
+            {
+                Loai = "Kem";
+            }
+            Console.WriteLine(" MSSV: {0} | HoTen: {1} | Khoa: {2} | DiemTB: {3} | Xep loai: {4} ", studentID, fullName, faculty, averageScore, loai);
+        }
+    }
+}
